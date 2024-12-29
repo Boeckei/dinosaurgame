b8a9bfe375083d435ceb7ac3f4fb8689fa98bd22
Boeckei <simon.seeboeck@web.de>
2024-12-29 19:00:26 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,7 +9,7 @@ input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
     game.setLife(10)
     basic.showString("Los!")
     Dino = game.createSprite(1, 4)
-    for (let index = 0; index < 1; index++) {
+    for (let index = 0; index < 50; index++) {
         Kaktus = game.createSprite(4, 4)
         basic.pause(500)
         Kaktus.change(LedSpriteProperty.X, -1)
