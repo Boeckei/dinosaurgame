47619975ed5e4b4843851d0fa79be9d5bcebed17
Boeckei <simon.seeboeck@web.de>
2024-12-29 18:00:24 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,31 @@
+let Dino: game.LedSprite = null
+let Kaktus: game.LedSprite = null
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    Dino.change(LedSpriteProperty.Y, -1)
+    basic.pause(1000)
+    Dino.change(LedSpriteProperty.Y, 1)
+})
+input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
+    game.setLife(10)
+    Dino = game.createSprite(1, 4)
+    for (let index = 0; index < 50; index++) {
+        Kaktus = game.createSprite(4, 4)
+        basic.pause(500)
+        Kaktus.change(LedSpriteProperty.X, -1)
+        basic.pause(500)
+        Kaktus.change(LedSpriteProperty.X, -1)
+        basic.pause(500)
+        if (Dino.isTouching(Kaktus)) {
+            game.removeLife(1)
+        }
+        Kaktus.change(LedSpriteProperty.X, -1)
+        if (Dino.isTouching(Kaktus)) {
+            game.removeLife(1)
+        }
+        basic.pause(500)
+        Kaktus.change(LedSpriteProperty.X, -1)
+        basic.pause(500)
+        Kaktus.change(LedSpriteProperty.X, -1)
+        Kaktus.delete()
+    }
+})
